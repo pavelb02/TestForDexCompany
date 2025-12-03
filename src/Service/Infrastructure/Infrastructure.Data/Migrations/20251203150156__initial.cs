39c84d596e84c0ac9fd9f5eed14f64d7763e105f
Pavel Balan <pavlik17122002@gmail.com>
2025-12-03 18:43:00 +0200

    develop feat: Добавить миграции.

---
+++ src/Service/Infrastructure/Infrastructure.Data/Migrations/20251203150156__initial.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "advertisements",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    number = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    text = table.Column<string>(type: "character varying(4000)", maxLength: 4000, nullable: false),
+                    image = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    rating = table.Column<double>(type: "double precision", nullable: false),
+                    start_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    end_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_advertisements", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_advertisements_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_advertisements_UserId",
+                table: "advertisements",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "advertisements");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
---
+++ src/Service/Infrastructure/Infrastructure.Data/Migrations/20251203163107__initial1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _initial1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId1",
+                table: "advertisements",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_advertisements_UserId1",
+                table: "advertisements",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_advertisements_users_UserId1",
+                table: "advertisements",
+                column: "UserId1",
+                principalTable: "users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_advertisements_users_UserId1",
+                table: "advertisements");
+
+            migrationBuilder.DropIndex(
+                name: "IX_advertisements_UserId1",
+                table: "advertisements");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "advertisements");
+        }
+    }
+}
